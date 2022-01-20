dda795197b6569776f44eccd409260d307f23260
chenpengfei <2578925789@@qq.com>
2022-01-20 15:16:26 +0800

    新增替换LN,GELU为BN,RELU

--- nanodet/model/backbone/convnext.py
+++ nanodet/model/backbone/convnext.py
@@ -77,6 +77,33 @@ class Block(nn.Module):
         x = input + self.drop_path(x)
         return x
 
+class Block_BN_RELU(nn.Module):
+    """
+    (1) DwConv -> LayerNorm (channels_first) -> 1x1 Conv -> GELU -> 1x1 Conv; all in (N, C, H, W)
+    """
+    def __init__(self, dim, drop_path=0., layer_scale_init_value=1e-6):
+        super().__init__()
+        self.dwconv = nn.Conv2d(dim, dim, kernel_size=7, padding=3, groups=dim)  # depthwise conv
+        self.act = nn.ReLU()
+        self.norm = nn.BatchNorm2d(dim)
+        self.pwconv1 = nn.Conv2d(dim, dim*4, kernel_size=1, stride=1)
+        self.pwconv2 = nn.Conv2d(dim*4, dim, kernel_size=1, stride=1)
+        self.gamma = nn.Parameter(layer_scale_init_value * torch.ones((dim,1,1)),
+                                  requires_grad=True) if layer_scale_init_value > 0 else None
+        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
+
+    def forward(self, x):
+        input = x
+        x = self.dwconv(x)
+        x = self.norm(x)
+        x = self.pwconv1(x)
+        x = self.act(x)
+        x = self.pwconv2(x)
+        if self.gamma is not None:
+            x = self.gamma * x
+
+        x = input + self.drop_path(x)
+        return x
 
 class ConvNeXt(nn.Module):
     r""" ConvNeXt
@@ -124,6 +151,102 @@ class ConvNeXt(nn.Module):
             cur += depths[i]
 
         self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
+        # self.head = nn.Linear(dims[-1], num_classes)
+
+        self.apply(self._init_weights)
+        # self.head.weight.data.mul_(head_init_scale)
+        # self.head.bias.data.mul_(head_init_scale)
+        self.out_stages = out_stages
+        self.init_weights(pretrain=pretrain)
+
+    def _init_weights(self, m):
+        if isinstance(m, (nn.Conv2d, nn.Linear)):
+            trunc_normal_(m.weight, std=.02)
+            nn.init.constant_(m.bias, 0)
+
+    def forward_features(self, x):
+        output = []
+        for i in range(4):
+            x = self.downsample_layers[i](x)
+            x = self.stages[i](x)
+            if i in self.out_stages:
+                output.append(x)
+        # return self.norm(x.mean([-2, -1])) # global average pooling, (N, C, H, W) -> (N, C)
+        return tuple(output)
+    def forward(self, x):
+        x = self.forward_features(x)
+        # x = self.head(x)
+        return x
+
+    def init_weights(self, pretrain=True):
+        if pretrain:
+            url = model_urls['convnext_tiny_1k']
+            checkpoint = torch.hub.load_state_dict_from_url(url=url, map_location="cpu", check_hash=True)
+            print("=> loading pretrained model {}".format(url))
+            self.load_state_dict(checkpoint, strict=False)
+        else:
+            for m in self.modules():
+                if self.activation == "LeakyReLU":
+                    nonlinearity = "leaky_relu"
+                else:
+                    nonlinearity = "relu"
+                if isinstance(m, nn.Conv2d):
+                    nn.init.kaiming_normal_(
+                        m.weight, mode="fan_out", nonlinearity=nonlinearity
+                    )
+                elif isinstance(m, nn.BatchNorm2d):
+                    m.weight.data.fill_(1)
+                    m.bias.data.zero_()
+
+class ConvNeXt_BN_RELU(nn.Module):
+    r""" ConvNeXt
+        A PyTorch impl of : `A ConvNet for the 2020s`  -
+          https://arxiv.org/pdf/2201.03545.pdf
+
+    Args:
+        in_chans (int): Number of input image channels. Default: 3
+        num_classes (int): Number of classes for classification head. Default: 1000
+        depths (tuple(int)): Number of blocks at each stage. Default: [3, 3, 9, 3]
+        dims (int): Feature dimension at each stage. Default: [96, 192, 384, 768]
+        drop_path_rate (float): Stochastic depth rate. Default: 0.
+        layer_scale_init_value (float): Init value for Layer Scale. Default: 1e-6.
+        head_init_scale (float): Init scaling value for classifier weights and biases. Default: 1.
+    """
+    def __init__(self, in_chans=3, out_stages=(1, 2, 3, 4),
+                 depths=[3, 3, 9, 3],
+                 dims=[96, 192, 384, 768], drop_path_rate=0.,
+                 layer_scale_init_value=1e-6, head_init_scale=1., pretrain=True
+                 ):
+        super().__init__()
+
+        self.downsample_layers = nn.ModuleList() # stem and 3 intermediate downsampling conv layers
+        stem = nn.Sequential(
+            nn.Conv2d(in_chans, dims[0], kernel_size=4, stride=4),
+            # LayerNorm(dims[0], eps=1e-6, data_format="channels_first"),
+            nn.BatchNorm2d(dims[0])
+        )
+        self.downsample_layers.append(stem)
+        for i in range(3):
+            downsample_layer = nn.Sequential(
+                    # LayerNorm(dims[i], eps=1e-6, data_format="channels_first"),
+                    nn.BatchNorm2d(dims[i]),
+                    nn.Conv2d(dims[i], dims[i+1], kernel_size=2, stride=2),
+            )
+            self.downsample_layers.append(downsample_layer)
+
+        self.stages = nn.ModuleList() # 4 feature resolution stages, each consisting of multiple residual blocks
+        dp_rates=[x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))]
+        cur = 0
+        for i in range(4):
+            stage = nn.Sequential(
+                *[Block_BN_RELU(dim=dims[i], drop_path=dp_rates[cur + j],
+                layer_scale_init_value=layer_scale_init_value) for j in range(depths[i])]
+            )
+            self.stages.append(stage)
+            cur += depths[i]
+
+        # self.norm = nn.LayerNorm(dims[-1], eps=1e-6) # final norm layer
+        self.norm = nn.BatchNorm2d(dims[-1])  # final norm layer
         # self.head = nn.Linear(dims[-1], num_classes)
 
         self.apply(self._init_weights)
